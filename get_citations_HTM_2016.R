f77de74df708418dfd9c0c78506f55dfbf651868
Bernd Klaus <bernd.klaus@embl.de>
2016-10-10 16:12:11 +0200

    added section on tidy data and reshaping between long and wide format

--- get_citations_HTM_2016.R
+++ get_citations_HTM_2016.R
@@ -1,5 +1,6 @@
 library(knitcitations)
 
+library(bibtex)
 
 # Neuman et. al., 2010
 citep("10.1038/nature08869")
@@ -7,4 +8,9 @@ citep("10.1038/nature08869")
 # Sommer et. al., 2013
 citep("10.1093/bioinformatics/btt175")
 
+# Wichkham, tidy data
+
+citep("https://www.jstatsoft.org/article/view/v059i10")
+
 write.bibtex(file = "HTM_2016.bib")
+
